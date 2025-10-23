eace28d333eb3e31137a7dade702cf14616c12a7
DavidCheshenko <davidcesenko@gmail.com>
2025-10-23 13:05:59 +0200

    H-1 Создать проект "Helper"

---
+++ Helper/Helper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Helper
+//
+//  Created by David Cheshenko on 22.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
