2939ea4ce3242f9aec5812082a4dda98afc2ee80
David Cheshenko <161839521+davidcheshenko@users.noreply.github.com>
2025-12-19 17:48:09 +0100

    H-4 Добавить папки в корень проекта

---
+++ Helper/Map/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Helper
+//
+//  Created by David Cheshenko on 19.12.25.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
