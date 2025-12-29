e66b6ef1fcd98563435e004193938294e4a2cfc1
DavidCheshenko <davidcesenko@gmail.com>
2025-12-29 16:22:25 +0100

    H-8 Добавить ViewModel's в проект

--- Helper/Profile/ProfileViewController.swift
+++ Helper/Profile/ProfileViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    // MARK: Private properties
+    
+    private let viewModel: IProfileViewModel
+    
+    // MARK: Lifecycle
+    
+    init(viewModel: IProfileViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Public methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
