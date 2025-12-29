7cba60354c1d6a19d960004f91c98ac9b66103e0
David Cheshenko <161839521+davidcheshenko@users.noreply.github.com>
2025-12-29 16:35:54 +0100

    H-8 Добавить ViewModel's в проект

--- Helper/Weather/WeatherViewController.swift
+++ Helper/Weather/WeatherViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
+    // MARK: Private properties
+    
+    private let viewModel: IWeatherViewModel
+    
+    // MARK: Lifecycle
+    
+    init(viewModel: IWeatherViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Public methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
+
