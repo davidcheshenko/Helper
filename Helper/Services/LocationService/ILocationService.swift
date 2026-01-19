585fe5f726fd128acb6fcf61f07338c9b889d166
DavidCheshenko <davidcesenko@gmail.com>
2026-01-19 19:59:48 +0100

    H-15 Создать LocationService

---
+++ Helper/Services/LocationService/ILocationService.swift
@@ -0,0 +1,14 @@
+//
+//  ILocationService.swift
+//  Helper
+//
+//  Created by David Cheshenko on 19.01.26.
+//
+
+import Foundation
+import CoreLocation
+
+protocol ILocationService {
+    
+    func getCurrentUserLocation() -> CLLocation
+}
---
+++ Helper/Services/LocationService/LocationService.swift
@@ -0,0 +1,16 @@
+//
+//  LocationService.swift
+//  Helper
+//
+//  Created by David Cheshenko on 19.01.26.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationService: ILocationService {
+    
+    func getCurrentUserLocation() -> CLLocation {
+        .init()
+    }
+}
