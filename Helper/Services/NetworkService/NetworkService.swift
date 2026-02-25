3eb4396d7692856b31234fcf2598a5fad739bb55
David Cheshenko <161839521+davidcheshenko@users.noreply.github.com>
2026-02-25 16:00:21 +0100

    H-27 Создать NetworkService

---
+++ Helper/Services/NetworkService/NetworkService.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkService.swift
+//  Helper
+//
+//  Created by David Cheshenko on 23.02.26.
+//
+
+import Foundation
+
+final class NetworkService: INetworkService {
+    
+    func getNews() {
+        // TODO: -
+    }
+}
