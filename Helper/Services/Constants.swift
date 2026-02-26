c0f385dcb471303d1d3963589889657633fe0231
DavidCheshenko <davidcesenko@gmail.com>
2026-02-26 11:43:05 +0100

    commit

--- Helper/Services/Constants.swift
+++ Helper/Services/Constants.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 class Constants {
-    static let apiKey = "41642d6fe8e14918b2e92d254bde5cd0"
-    static let baseUrl = "https://newsapi.org/v2/"
+    static let apiKeyNews = "41642d6fe8e14918b2e92d254bde5cd0"
+    static let newsBaseUrl = "https://newsapi.org/v2/"
 }
