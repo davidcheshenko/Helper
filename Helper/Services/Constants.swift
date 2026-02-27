6f88af193139b588c07f8fa39abdbc65a5e3dd35
David Cheshenko <161839521+davidcheshenko@users.noreply.github.com>
2026-02-27 22:30:48 +0100

    H-28 Добавить APIKey и BaseURL

---
+++ Helper/Services/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Helper
+//
+//  Created by David Cheshenko on 26.02.26.
+//
+
+import Foundation
+
+class Constants {
+    static let apiKeyNews = "41642d6fe8e14918b2e92d254bde5cd0"
+    static let newsBaseUrl = "https://newsapi.org/v2/"
+}
