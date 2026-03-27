fa8f1bb9590274d28a0442bc6f319c210f40deb6
DavidCheshenko <davidcesenko@gmail.com>
2026-03-27 18:49:38 +0100

    no message

--- Helper/Models/NewsResponse.swift
+++ Helper/Models/NewsResponse.swift
@@ -8,23 +8,14 @@
 import Foundation
 
 struct NewsResponse: Codable {
-    let status: String?
-    let totalResults: Int
     let articles: [Article]
 }
 
-struct Article: Codable {
-    let source: Source
-    let author: String?
+struct Article: Codable, Identifiable {
+    var id: String? { url }
     let title: String?
     let description: String?
     let url: String?
     let urlToImage: String?
-    let publishedAt: String?
-    let content: String?
 }
 
-struct Source: Codable {
-    let id: String?
-    let name: String?
-}
