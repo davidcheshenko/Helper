4771cf4273e685bff5dabf090d55bff7676f8a91
David Cheshenko <161839521+davidcheshenko@users.noreply.github.com>
2026-03-28 00:42:47 +0100

    H-29 Создать модель данных новостей
    * H-29 Создать модель данных новостей
    
    * H-29 Создать модель данных новостей
    
    * no message
    
    * no message
    
    * no message
    
    * no message

---
+++ Helper/Models/NewsResponse.swift
@@ -0,0 +1,20 @@
+//
+//  NewsResponse.swift
+//  Helper
+//
+//  Created by David Cheshenko on 25.03.26.
+//
+
+import Foundation
+
+struct NewsResponse: Codable {
+    let articles: [Article]?
+}
+
+struct Article: Codable {
+    let title: String?
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+}
+
