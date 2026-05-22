8023793e7134f3dca5584bab565a7092071bfe2e
DavidCheshenko <davidcesenko@gmail.com>
2026-05-22 11:29:16 +0200

    no message

--- Helper/Constants.swift
+++ Helper/Constants.swift
@@ -14,7 +14,7 @@ typealias WeatherCompletion = (Result<WeatherResponse, Error>) -> Void
 class Constants {
     static let apiKeyNews = "41642d6fe8e14918b2e92d254bde5cd0"
     static let newsBaseUrl = "https://newsapi.org/v2/"
-    static let identifier = "NewsCell"
+    static let NewsCellID = "NewsCell"
     static let apiKeyWeather = "4d920f27707f0d437a34c4db31b5cd1a"
     static let weatherBaseUrl = "https://api.openweathermap.org/data/2.5/"
 }
