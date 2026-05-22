f054cd9326a1f5adcede65d3a5d899a5df7d9d8b
David Cheshenko <161839521+davidcheshenko@users.noreply.github.com>
2026-05-22 11:37:17 +0200

    H 36
    * H-36 Создать модель данных погоды
    
    * no message
    
    * no message

--- Helper/Constants.swift
+++ Helper/Constants.swift
@@ -9,10 +9,13 @@ import Foundation
 
 typealias ResultCompletion = (Result<Data, Error>) -> ()
 typealias NewsCompletion = (Result<[Article]?, Error>) -> ()
+typealias WeatherCompletion = (Result<WeatherResponse, Error>) -> Void
 
 class Constants {
     
     static let apiKeyNews = "41642d6fe8e14918b2e92d254bde5cd0"
     static let newsBaseUrl = "https://newsapi.org/v2/"
     static let NewsCellID = "NewsCell"
+    static let apiKeyWeather = "4d920f27707f0d437a34c4db31b5cd1a"
+    static let weatherBaseUrl = "https://api.openweathermap.org/data/2.5/"
 }
---
+++ Helper/Models/WeatherResponse.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherResponse.swift
+//  Helper
+//
+//  Created by David Cheshenko on 21.05.26.
+//
+
+import Foundation
+
+struct WeatherResponse: Codable {
+    let name: String?
+    let main: Main?
+    let weather: [Weather]?
+}
+
+struct Main: Codable {
+    let temp: Double?
+}
+
+struct Weather: Codable {
+    let discription: String?
+}
