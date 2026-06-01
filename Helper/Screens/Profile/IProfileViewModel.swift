e59a3d927ac661ede06aa4f33c5540356124ef68
DavidCheshenko <davidcesenko@gmail.com>
2026-06-01 22:03:18 +0200

    H-38 Создать экран Profile

--- Helper/Screens/Profile/IProfileViewModel.swift
+++ Helper/Screens/Profile/IProfileViewModel.swift
@@ -9,4 +9,12 @@ import Foundation
 
 protocol IProfileViewModel {
     
+    var userName: String { get }
+    var userEmail: String { get }
+    var userPassword: String { get }
+    
+    var onDataChanged: (() -> Void)? { get set }
+    var onErrorOccurrad: ((String) -> Void)? { get set }
+    
+    func saveProfile (name: String, email: String, password: String)
 }
--- Helper/Screens/Profile/ProfileViewModel.swift
+++ Helper/Screens/Profile/ProfileViewModel.swift
@@ -9,4 +9,48 @@ import Foundation
 
 class ProfileViewModel: IProfileViewModel {
     
+    private enum Keys {
+        static let name = "user_name_key"
+        static let email = "user_email_key"
+        static let password = "user_password_key"
+    }
+    
+    var userName: String { profile.name }
+    var userEmail: String { profile.email }
+    var userPassword: String { profile.password }
+    
+    var onDataChanged: (() -> Void)?
+    var onErrorOccurrad: ((String) -> Void)?
+    
+    private var profile: Profile {
+        didSet { onDataChanged?() }
+    }
+    
+    init(profile: Profile) {
+        let savedName = UserDefaults.standard.string(forKey: Keys.name) ?? ""
+        let savedEmail = UserDefaults.standard.string(forKey: Keys.email) ?? ""
+        let savedPassword = UserDefaults.standard.string(forKey: Keys.password) ?? ""
+        
+        if !savedName.isEmpty || !savedEmail.isEmpty || !savedPassword.isEmpty {
+            self.profile = Profile(name: savedName, email: savedEmail, password: savedPassword)
+        } else {
+            
+            self.profile = profile
+        }
+    }
+    
+    func saveProfile(name: String, email: String, password: String) {
+        let trimmedName = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        let trimmedEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if trimmedName.isEmpty {
+            onErrorOccurrad? ("имя не может быть пустым")
+            return
+        }
+        
+        if password.count < 6 {
+            onErrorOccurrad? ("Пороль должен содержать минимум 6 символов")
+            return
+        }
+    }
 }
